d392093108178326e012cfa8cc3d274538660278
vkolesnik017 <vkolesnik017@gmail.com>
2019-06-23 02:24:24 +0300

    Commit1

--- src/test/java/test/Booking.java
+++ src/test/java/test/Booking.java
@@ -5,48 +5,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Booking {
-
+    WebDriver driver = new ChromeDriver();
 @Test
-    public void Test () throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-       driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-        driver.get("https://www.booking.com");
-    WebElement city_field=driver.findElement(By.xpath("//input[@id='ss']"));
-    city_field.sendKeys("Рим");
-    WebElement dropMenu=driver.findElement(By.xpath("//li[contains(@class,'c-autocomplete__item sb-autocomplete__item sb-autocomplete__item-with_photo sb-autocomplete__item--airport sb-autocomplete__item__item--elipsis')] "));
-    dropMenu.click();
-    TimeUnit.SECONDS.sleep(3);
-    WebElement dayDeparture=driver.findElement(By.xpath("//div[contains(@class,'bui-calendar__main')]//div[1]//table[1]//tbody[1]//tr[6]//td[7]"));
-    dayDeparture.click();
-    TimeUnit.SECONDS.sleep(2);
-    WebElement dayArrival=driver.findElement(By.xpath("//div[@class='bui-calendar__content']/div[2]/table/tbody/tr[2]/td[7]"));
-    dayArrival.click();
-    WebElement buttonCheckPrice=driver.findElement(By.xpath("//div[contains(@class,'sb-searchbox-submit-col -submit-button')]//button[contains(@class,'')]"));
-    buttonCheckPrice.click();
-    TimeUnit.SECONDS.sleep(3);
-    WebElement closeMap=driver.findElement(By.xpath("//div[@class='map_full_overlay__close']"));
-    closeMap.click();
-    WebElement sortMinPrice=driver.findElement(By.xpath("//li[contains(@class,'sort_price')]"));
-    sortMinPrice.click();
-    WebElement apartments=driver.findElement(By.xpath("//div[contains(@class,'sr_header--title')]//div[contains(@class,'sr_header')]"));
-    String total=(apartments.getText()).replaceAll("[^0-9]", "");
+    public void testApartmentsSortingByPrice () throws InterruptedException {
+    BookingPage page = PageFactory.initElements(driver, test.BookingPage.class);
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    page.cityField.sendKeys("Rome");
+    page.dropMenu.click();
+    page.dayDeparture.click();
+    page.dayArrival.click();
+    page.buttonCheckPrice.click();
+    page.closeMap.click();
+    page.sortMinPrice.click();
+    String total = (page.apartments.getText()).replaceAll("[^0-9]", "");
     System.out.println("Total apartments: " + total);
-    TimeUnit.SECONDS.sleep(2);
-
-
- /*   WebElement priceOfApartment=driver.findElement(By.xpath("//div[@class='prco-wrapper bui-price-display prco-sr-default-assembly-wrapper prc-d-sr-wrapper']/div[2]/div"));
-    String minPrice= (priceOfApartment.getText()).replaceAll("[^0-9]", "");
-    System.out.println("The price of the cheapest apartment: " +minPrice+ " UAH");*/
-
-    List<WebElement> price = driver.findElements(By.xpath("//div[@class='prco-wrapper bui-price-display prco-sr-default-assembly-wrapper prc-d-sr-wrapper']/div[2]/div"));
- //   price.size();
-    System.out.println(price.size());
-
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    TimeUnit.SECONDS.sleep(3);
+    page.minPriceApartment();
+    driver.quit();
     }
 }
---
+++ src/test/java/test/BookingPage.java
@@ -0,0 +1,63 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BookingPage {
+
+    WebDriver driver;
+
+    @FindBy(how = How.XPATH, xpath = "//input[@id='ss']")
+    WebElement cityField;
+    @FindBy(how = How.XPATH, xpath = "//li[contains(@class,'c-autocomplete__item sb-autocomplete__item sb-autocomplete__item-with_photo sb-autocomplete__item--airport sb-autocomplete__item__item--elipsis')]")
+    WebElement dropMenu;
+    @FindBy(how = How.XPATH, xpath = "//div[contains(@class,'bui-calendar__main')]//div[1]//table[1]//tbody[1]//tr[6]//td[7]")
+    WebElement dayDeparture;
+    @FindBy(how = How.XPATH, xpath = "//div[@class='bui-calendar__content']/div[2]/table/tbody/tr[2]/td[7]")
+    WebElement dayArrival;
+    @FindBy(how = How.XPATH, xpath = "//div[contains(@class,'sb-searchbox-submit-col -submit-button')]//button[contains(@class,'')]")
+    WebElement buttonCheckPrice;
+    @FindBy(how = How.XPATH, xpath = "//div[@class='map_full_overlay__close']")
+    WebElement closeMap;
+    @FindBy(how = How.XPATH, xpath = "//li[contains(@class,'sort_price')]")
+    WebElement sortMinPrice;
+    @FindBy(how = How.XPATH, xpath = "//div[contains(@class,'sr_header--title')]//div[contains(@class,'sr_header')]")
+    WebElement apartments;
+
+    public BookingPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get("https://www.booking.com");
+    }
+
+    public void minPriceApartment() throws InterruptedException {
+
+        List<WebElement> allPriceElements = driver.findElements(By.xpath("//div[contains(@class, 'price-display__value')]"));
+        List<Integer> allPrices = new ArrayList<Integer>();
+        for (WebElement e : allPriceElements) {
+            if (!e.getText().isEmpty()) {
+                String price = e.getText().replaceAll("[^0-9]", "");
+                allPrices.add(Integer.parseInt(price));
+            }
+        }
+
+        List<Integer> sortPrice = new ArrayList<Integer>(allPrices);
+        Collections.sort(sortPrice);
+        sortList(sortPrice, allPrices);
+    }
+
+    public void sortList(List<Integer> first, List<Integer> second) {
+        if (second.equals(first)) {
+            System.out.println("Price sorting works correctly");
+        } else {
+            System.out.println("Price sorting doesn't work correctly");
+        }
+    }
+
+}
